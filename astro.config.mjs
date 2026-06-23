8cf1d11492e98ded913a4c8a6ae44b9502770e4c
Pedro Olvera <pedro.olvera.8a@gmail.com>
2026-06-23 11:50:19 -0600

    JAla

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-
-export default defineConfig({
-  output: "static",
-});
+export default defineConfig({});
